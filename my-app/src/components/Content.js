ad578ab59d2545e0d300fbdf6726aa384fe7bf7c
Jean de Villiers <jeandevilliers@me.com>
2023-05-28 15:41:49 +0000

    Complete lifecycles challenge

--- my-app/src/components/Content.js
+++ my-app/src/components/Content.js
@@ -2,15 +2,29 @@ import React, { Component } from 'react'
 import css from './css/Content.module.css'
 import {savedPosts} from '../posts.json'
 import PostItem from './PostItem'
+import Loader from './Loader'
 
 export class Content extends Component {
     constructor(props) {
       super(props)
     
       this.state = {
-        
+        isLoaded: false,
       }
     }
+    getData() {
+      console.log('getData() called')
+      setTimeout(() => {
+          console.log('Data fetched!')
+          this.setState({
+              isLoaded: true
+          })
+      }, 2000)
+  }
+  componentDidMount() {
+      console.log('Component mounted')
+      this.getData()
+  }
   render() {
     return (
       <div className={css.Content}>
@@ -18,8 +32,13 @@ export class Content extends Component {
             <h1>My Photos</h1>
         </div>
         <div className={css.SearchResults}>
-            <PostItem post={savedPosts} />
+        {this.state.isLoaded ? (
+          <PostItem post={savedPosts} />
+          ) : (
+            <Loader />
+            )}
         </div>
+        
       </div>
     )
   }
