61462b67c3768fe6adec7b1fa71b69d3f42cb22c
Jean de Villiers <jeandevilliers@me.com>
2023-05-28 17:24:23 +0000

    Start search bar challenge

--- my-app/src/components/Content.js
+++ my-app/src/components/Content.js
@@ -10,6 +10,7 @@ export class Content extends Component {
     
       this.state = {
         isLoaded: false,
+        posts: []
       }
     }
     getData() {
@@ -21,15 +22,42 @@ export class Content extends Component {
           })
       }, 2000)
   }
+
   componentDidMount() {
       console.log('Component mounted')
       this.getData()
+      this.setState({
+        posts: {savedPosts}
+      })
   }
+
+  handleChange = (event) => {
+    const inputText = event.target.value.toLowerCase()
+    console.log(inputText)
+    const filteredPosts = {savedPosts}.filter(post => {
+        return post.name.toLowerCase().includes(inputText)
+    })
+    this.setState({
+        posts: filteredPosts
+    })
+}
+
   render() {
     return (
       <div className={css.Content}>
         <div className={css.TitleBar}>
             <h1>My Photos</h1>
+            <form>
+              <label
+              htmlFor="searchinput"
+              >Search:</label>
+              <input
+              onChange={(event) => this.handleChange(event)}
+              type="search"
+              id="searchinput"
+              ></input>
+              <h4>posts found: {this.state.posts.length}</h4>
+            </form>
         </div>
         <div className={css.SearchResults}>
         {this.state.isLoaded ? (
