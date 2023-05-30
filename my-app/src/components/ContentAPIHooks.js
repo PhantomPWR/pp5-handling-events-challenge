bf2de6a0156bcf6eaf9e30135a59e5f706a53ab0
Jean de Villiers <jeandevilliers@me.com>
2023-05-30 19:22:06 +0000

    Replace content class component with hooks

--- my-app/src/components/ContentAPIHooks.js
+++ my-app/src/components/ContentAPIHooks.js
@@ -1,85 +1,63 @@
-import React, { Component } from 'react'
-import css from './css/Content.module.css'
-import PostItem from './PostItemAPI'
-import Loader from './Loader'
-import axios from 'axios'
-import API_KEY from '../secrets'
+import React, { useState, useEffect } from 'react';
+import css from './css/Content.module.css';
+import PostItem from './PostItemAPI';
+import Loader from './Loader';
+import axios from 'axios';
+import API_KEY from '../secrets';
 
-export class Content extends Component {
-  constructor(props) {
-    super(props)
-  
-    this.state = {
-      isLoaded: false,
-      posts: [],
-      savedPosts: []
-    }
-  }
+const Content = () => {
+  const [isLoaded, setIsLoaded] = useState(false);
+  const [posts, setPosts] = useState([]);
+  const [savedPosts, setSavedPosts] = useState([]);
 
-componentDidMount() {
-console.log('Component mounted')
-this.fetchImages()
-}
+  useEffect(() => {
+    console.log('Component mounted');
+    fetchImages();
+  }, []);
 
-fetchImages() {
-axios.get('https://pixabay.com/api/?key=' + API_KEY + '&per_page=100')
-.then(response => {
-  console.log(response);
-  const fetchedPosts = response.data.hits
-  this.setState({
-    posts: fetchedPosts,
-    savedPosts: fetchedPosts,
-    isLoaded: true
-  })
-})
+  const fetchImages = () => {
+    axios.get(`https://pixabay.com/api/?key=${API_KEY}&per_page=100`)
+      .then(response => {
+        console.log(response);
+        const fetchedPosts = response.data.hits;
+        setPosts(fetchedPosts);
+        setSavedPosts(fetchedPosts);
+        setIsLoaded(true);
+      });
+  };
 
-}
+  const handleChange = (event) => {
+    const name = event.target.value.toLowerCase();
+    console.log(name);
+    const filteredPosts = savedPosts.filter(post => {
+      return post.user.toLowerCase().includes(name);
+    });
+    setPosts(filteredPosts);
+  };
 
-handleChange = (event) => {
-const name = event.target.value.toLowerCase()
-console.log(name)
-const filteredPosts = this.state.savedPosts.filter(post => {
-    return post.user.toLowerCase().includes(name)
-})
-this.setState({
-    posts: filteredPosts
-})
-
-}
-
-
-
-render() {
   return (
     <div className={css.Content}>
       <div className={css.TitleBar}>
-          <h1>My Photos</h1>
-          <form>
-            <label
-            htmlFor="searchinput"
-            >Search:</label>
-            <input
-            onChange={(event) => this.handleChange(event)}
+        <h1>My Photos</h1>
+        <form>
+          <label htmlFor="searchinput">Search:</label>
+          <input
+            onChange={handleChange}
             type="search"
             id="searchinput"
-            ></input>
-            <h4>posts found: {this.state.posts.length}</h4>
-          </form>
+          />
+          <h4>posts found: {posts.length}</h4>
+        </form>
       </div>
       <div className={css.SearchResults}>
-      {
-        this.state.isLoaded ? (
-          <PostItem post={this.state.posts} />
-          ) : (
-            <Loader />
-            )
-      }
-
+        {isLoaded ? (
+          <PostItem post={posts} />
+        ) : (
+          <Loader />
+        )}
       </div>
-      
     </div>
-  )
-}
-}
+  );
+};
 
-export default Content
+export default Content;
